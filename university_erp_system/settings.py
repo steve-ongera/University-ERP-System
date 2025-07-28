9d871ba64157bcc22f147b8b18f8f0400f5585ba
steve <gadafisteve001@gmail.com>
2025-07-28 16:10:01 +0300

    made updates

--- university_erp_system/settings.py
+++ university_erp_system/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'core_application',
 ]
 
+AUTH_USER_MODEL='core_application.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
