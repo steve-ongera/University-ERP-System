36405f2bbbf032be9f975e0f5e369c8c590bb8aa
steve <gadafisteve001@gmail.com>
2025-08-14 12:07:43 +0300

    made updates

--- core_application/urls.py
+++ core_application/urls.py
@@ -286,13 +286,13 @@ urlpatterns = [
 
     path('department-list/', views.department_list, name='department_list'),
     path('department/create/', views.department_create, name='department_create'),
-    path('department/<str:code>/', views.department_detail, name='department_detail'),
-    path('department/<str:code>/update/', views.department_update, name='department_update'),
-    path('department/<str:code>/delete/', views.department_delete, name='department_delete'),
-    path('department/export/', views.department_export, name='department_export'),
+    path('departments/<str:code>/', views.department_detail, name='department_detail'),
+    path('departments/<str:code>/update/', views.department_update, name='department_update'),
+    path('departments/<str:code>/delete/', views.department_delete, name='department_delete'),
+    path('departments/export/', views.department_export, name='department_export'),
     
     # AJAX endpoints
-    path('department/api/<int:department_id>/programmes/', views.get_department_programmes, name='get_department_programmes'),
+    path('departments/api/<int:department_id>/programmes/', views.get_department_programmes, name='get_department_programmes'),
 
 
 ]
