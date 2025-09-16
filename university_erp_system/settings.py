10a70280bf6fd730759f435f9fd47ed05835a8c4
steve <gadafisteve001@gmail.com>
2025-09-16 19:06:57 +0300

    added security models

--- university_erp_system/settings.py
+++ university_erp_system/settings.py
@@ -160,12 +160,15 @@ LOGO_URL = "/static/logo.png"
 
 # Email settings for security alerts
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'your-smtp-server.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your-email@domain.com'
-EMAIL_HOST_PASSWORD = 'your-email-password'
-DEFAULT_FROM_EMAIL = 'University Security System <security@yourdomain.com>'
+EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT = config("EMAIL_PORT", cast=int, default=587)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = config(
+    "DEFAULT_FROM_EMAIL",
+    default="University Security System <security@yourdomain.com>"
+)
 
 # Security settings
 SECURE_BROWSER_XSS_FILTER = True
